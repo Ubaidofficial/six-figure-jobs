3aa1acca4cfb148f89bb12023833316745e6e97c
Your Name <you@example.com>
2025-12-14 15:23:29 +0100

    Fix companies page module to satisfy Next.js build

---
+++ app/companies/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Companies hiring $100k+ roles',
+}
+
+export default function CompaniesPage() {
+  return null
+}
