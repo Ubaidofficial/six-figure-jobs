4114fbf7945deb3f6007907e86fa2baa9a7a24b7
Your Name <you@example.com>
2025-12-16 22:47:03 +0100

    fix(location): normalize locationRaw bullets + tighten multi-location detection

---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+}
